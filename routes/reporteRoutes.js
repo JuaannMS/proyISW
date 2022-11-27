c148137d2e735821b902924b77b48d9ebfeed270
ReneSanchezH <rene.sanchez2001@alumnos.ubiobio.cl>
2022-11-27 00:39:27 -0300

    nueva actualización de reportes

--- routes/reporteRoutes.js
+++ routes/reporteRoutes.js
@@ -3,8 +3,10 @@ const ReporteController = require('../controllers/reporteController');
 const api = express.Router();
 
 api.post('/reporte', ReporteController.createReporte);
-api.get('/reportes', ReporteController.getReportes);
 api.delete('/reporte/delete/', ReporteController.deleteReportes);
+api.get('/reporte/get/', ReporteController.getReporte)
+api.get('/reporte/getALL/', ReporteController.getALLReportes);
+api.put('/reporte/update/', ReporteController.updateReporte);
 
 
 module.exports = api
