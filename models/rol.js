382586433f10c4615501154d5c637d86a5c23984
juanmarin <jmms.android@gmail.com>
2022-12-08 12:49:31 -0300

    back y front

--- models/rol.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const rolSchema = new Schema({
-    name:{  type: String}
-});
-
-module.exports = mongoose.model('rol', rolSchema);
